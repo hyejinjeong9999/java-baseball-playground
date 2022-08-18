79f29634041de9b9ff0556743e0d4e88f9486ffc
hyejin <hyejinj810@gmail.com>
2022-08-19 05:15:34 +0900

    메인 구현

---
+++ src/main/java/main.java
@@ -0,0 +1,9 @@
+import controller.BaseBallGameController;
+
+public class main {
+
+    public static void main(String[] args) {
+        BaseBallGameController baseBallGameController = new BaseBallGameController();
+        baseBallGameController.game();
+    }
+}
